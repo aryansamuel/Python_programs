e55e452d000b25981682a9ac3f0bbb20a843d80d
Aryan Samuel <aryan.samuel0596@gmail.com>
2017-01-18 03:18:18 -0800

    Delete prac_concat.py

--- prac_concat.py
+++
@@ -1,9 +0,0 @@
-def main():
-    x = input('Please enter your first name: ')
-    y = input('Please enter your last name: ')
-    a = eval(input('Please enter the number of the month you were born: '))
-    list = ('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec')
-    z = x[0]+y[0:7]
-    lowerx = z.lower()
-    print(lowerx+list[a-1])
-main()
