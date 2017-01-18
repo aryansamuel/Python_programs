73a7b4aa57765564ffc804e2fdb0280dcebe6780
Aryan Samuel <aryan.samuel0596@gmail.com>
2017-01-18 03:18:54 -0800

    Delete temp_convert.py

--- temp_convert.py
+++
@@ -1,7 +0,0 @@
-def main():
-    print('Conver Celsius to Farhenheit')
-    c = eval(input('Enter the temperature in Celsius: '))
-    f = (9/5)*c+32
-    print ('The temperature in Farhenheit is',f)
-
-main()
